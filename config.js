3a2caafe369159b4555605a799ca5a3b68b229d4
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-06-30 21:14:12 -0300

    Fix

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default ({
   theme: 'themes/blog.html',
   lang: '',
   title: 'Blog Post',
-  base: '/hippo',
+  base: 'https://marcodpt.github.io/hippo/',
   dir: 'docs',
   sort: ['index', '-date', '-size'],
   taxonomies: ['tags', 'authors'],
