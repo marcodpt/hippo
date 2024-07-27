f50c8f61536436df7a4e91181f9f528626b4a66b
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-07-27 14:39:09 -0300

    added theme selector to blog

--- build/blog.js
+++ build/blog.js
@@ -9,6 +9,7 @@ export default ({
   dir: 'docs/blog',
   rss: true,
   settings: {
+    style: '',
     nav: [
       {
         title: 'Home',
@@ -25,6 +26,60 @@ export default ({
         title: 'Rss Feed',
         url: 'https://marcodpt.github.io/hippo/blog/rss.xml',
         last: true
+      }
+    ],
+    themes: [
+      {
+        href: 'javascript:setTheme(\'\')',
+        title: 'Browser Default'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css\')',
+        title: 'Water Css Light'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css\')',
+        title: 'Water Css Dark'
+      }, {
+        href: 'javascript:setTheme(\'https://unpkg.com/mvp.css\')',
+        title: 'MVP.css'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/sakura.css/css/sakura.css\')',
+        title: 'Sakura Default'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/sakura.css/css/sakura-earthly.css\')',
+        title: 'Sakura Earthly'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/sakura.css/css/sakura-vader.css\')',
+        title: 'Sakura Vader'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/sakura.css/css/sakura-dark.css\')',
+        title: 'Sakura Dark'
+      }, {
+        href: 'javascript:setTheme(\'https://unpkg.com/marx-css/css/marx.min.css\')',
+        title: 'Marx'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css.min.css\')',
+        title: 'Tacit'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.simplecss.org/simple.css\')',
+        title: 'Simple'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css\')',
+        title: 'Pico.css'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css\')',
+        title: 'Pure'
+      }, {
+        href: 'javascript:setTheme(\'https://unpkg.com/chota\')',
+        title: 'Chota'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/picnic@7.1.0/picnic.min.css\')',
+        title: 'Picnic'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\')',
+        title: 'Bootstrap'
+      }, {
+        href: 'javascript:setTheme(\'https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css\')',
+        title: 'Bulma'
       }
     ]
   }
